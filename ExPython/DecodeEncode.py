c4788f2748f02b09e4f1385c24155c82c81e521b
xs.zhou <newlifechou@outlook.com>
2017-12-18 14:38:08 +0800

    encode gb2132

--- ExPython/DecodeEncode.py
+++ ExPython/DecodeEncode.py
@@ -1,4 +1,7 @@
-s='苟以国家生死已'
+s='苟以国家生死已，岂因福祸避趋之'
 k=s.encode()
 print(k)
 print(k.decode())
+gbk=s.encode(encoding='gb2312')
+print(gbk)
+print(s)
--- ExPython/createmd5.py
+++ ExPython/createmd5.py
@@ -7,7 +7,7 @@ for line in s:
     m.update(line.encode())
     print(m.hexdigest())
 
-data='苟以国家生死已'
+data='苟以国家生死已,岂因福祸避趋之'
 m.update(data.encode(encoding='gb2312'))
 print(m.hexdigest())
 m.update(data.encode(encoding='utf_8'))
