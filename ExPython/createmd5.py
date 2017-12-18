b4fcf2ab609e085b6c8b7daab4fb284ffb88a40b
xs.zhou <newlifechou@outlook.com>
2017-12-18 14:34:23 +0800

    create md5 in python3

---
+++ ExPython/createmd5.py
@@ -0,0 +1,14 @@
+#use hashlib
+import hashlib
+m=hashlib.md5()
+
+s=['123','456','789']
+for line in s:
+    m.update(line.encode())
+    print(m.hexdigest())
+
+data='苟以国家生死已'
+m.update(data.encode(encoding='gb2312'))
+print(m.hexdigest())
+m.update(data.encode(encoding='utf_8'))
+print(m.hexdigest())
