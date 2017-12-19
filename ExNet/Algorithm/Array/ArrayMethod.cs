5e335a895e257168255f0f38a94c63b337d56441
xs.zhou <newlifechou@outlook.com>
2017-12-19 09:43:35 +0800

    array findmax

---
+++ ExNet/Algorithm/Array/ArrayMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Array
+{
+    public class ArrayMethod
+    {
+        public int FindMax(int[] data)
+        {
+            if (data.Length==0)
+            {
+                return -1;
+            }
+            int max = data[0];
+            for (int i = 1; i < data.Length; i++)
+            {
+                if (data[i]>max)
+                {
+                    max = data[i];
+                }
+            }
+            return max;
+        }
+    }
+}
