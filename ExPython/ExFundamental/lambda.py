c3c3b8957b5b27aadafb23ec9398580986f872e7
xs.zhou <newlifechou@outlook.com>
2017-12-21 16:38:10 +0800

    map and filter

--- ExPython/ExFundamental/lambda.py
+++ ExPython/ExFundamental/lambda.py
@@ -7,3 +7,13 @@ def test(f,a,b):
     print(f(a,b))
 
 test(lambda x,y:x-y,10,2)
+
+re=map(lambda x:x**2,[1,2,3,4])
+print(list(re))
+
+def func2(a):
+    return a % 2 == 0
+
+le=filter(func2,[1,2,3,4,5,6])
+for i in le:
+    print(i)
