e90d9770a00534968fa0c1641f850ec5a1fe9e2f
xs.zhou <newlifechou@outlook.com>
2017-12-22 11:46:30 +0800

    create a project for json

---
+++ ExNet/NetCore/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello xs.zhou!");
+	    Console.WriteLine("Fisrt Program of .NET Core");
+        }
+    }
+}
