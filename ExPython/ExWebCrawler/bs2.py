e37d085af4641c8c7ac56612102156bd85960532
xs.zhou <newlifechou@outlook.com>
2017-12-26 10:13:42 +0800

    use local html to test beautiful soup

---
+++ ExPython/ExWebCrawler/bs2.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+#import urllib.request
+#res=urllib.request.urlopen("http://www.cd-pmi.com")
+#html=res.read()
+#print(html)
+#读取文件的时候必须用这正确的编码读取，否则中文会乱码
+file=open("test.html","r",encoding="utf-8")
+html=""
+for line in file.readlines():
+    html=html+line
+file.close()
+#print(html)
+soup=BeautifulSoup(html,"html.parser")
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
---
+++ ExPython/ExWebCrawler/temp.py
@@ -0,0 +1,8 @@
+soup=BeautifulSoup(html,"html.parser")
+#print(soup.prettify())
+print(soup.title)
+print(soup.title.name)
+print(soup.title.string)
+print(soup.title.parent.name)
+print(soup.p)
+#print(soup.p['class'])
