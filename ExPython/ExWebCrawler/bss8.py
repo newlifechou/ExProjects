542c5ce874735fb6288a5b2eaf7c220318b0415f
xs.zhou <newlifechou@outlook.com>
2017-12-27 09:55:59 +0800

    basicaly know how to use beautifulsoup

--- ExPython/ExWebCrawler/bss8.py
+++ ExPython/ExWebCrawler/bss8.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 
 soup=BeautifulSoup(open("test.html","r",encoding="utf-8"),"html.parser")
 head_tag=soup.head
@@ -35,3 +36,37 @@ print("###############")
 print(title_tag.parent)
 print(title_tag.string.parent)
 print(soup.parent)
+print(p_soup)
+print(soup.p.next_sibling.next_sibling)
+
+print("===============")
+
+for sibling in p_soup.next_siblings:
+    print(sibling)
+
+first_p=soup.find("p",id="second")
+print(first_p)
+print(first_p.next_element)
+
+for i in soup.find_all("a"):
+    print(i)
+
+for p in soup.find_all("p"):
+    print(p)
+
+for tag in soup.find_all(re.compile("p")):
+    print(tag.string)
+
+
+for tag in soup.find_all(["p","a"]):
+    print(tag)
+print("$$$$$$$$$$$$$$$$$$$$$$")
+for t in soup.find_all(True):
+    print(t)
+
+
+def has_class_but_no_id(tag):
+    return tag.has_attr('class') and  not tag.has_attr('id')
+
+for t in soup.find_all(has_class_but_no_id):
+    print("====>",t)
