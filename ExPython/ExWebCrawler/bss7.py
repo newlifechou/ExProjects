072810e053aef364c123a70734b35e9fd5b0adac
xs.zhou <newlifechou@outlook.com>
2017-12-27 09:09:33 +0800

    print all

---
+++ ExPython/ExWebCrawler/bss7.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+soup=BeautifulSoup("<p>hello world</p>","html.parser")
+print(soup.p.string)
+
+unicode_string=soup.p.string.encode("utf-8")
+print(unicode_string)
+soup.p.string.replace_with("hello python")
+print(soup.p.string)
