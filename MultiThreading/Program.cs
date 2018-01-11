1b0f9653ccc000d7ad44f0d2a0bf771da8ddf102
xs.zhou <newlifechou@outlook.com>
2018-01-11 16:19:30 +0800

    create a simple thread

---
+++ MultiThreading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiThreading.Src;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleThread test = new SimpleThread();
+            test.One();
+
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ MultiThreading/Src/SimpleThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Src
+{
+    internal class SimpleThread
+    {
+        public void One()
+        {
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+            PrintLetters();
+        }
+
+
+        private void PrintNumbers()
+        {
+            Console.WriteLine("Starting");
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write(i);
+            }
+
+        }
+
+        private void PrintLetters()
+        {
+            Console.WriteLine("Starting");
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(200);
+                Console.Write("A");
+            }
+        }
+
+    }
+}
