f2ded26597079fbafde136b7ac9c5be61875fec1
xs.zhou <newlifechou@outlook.com>
2018-01-17 08:57:03 +0800

    add an oo project

---
+++ Examples_OO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examples_OO
+{
+    static class Program
+    {
+        static void Main()
+        {
+            father f = new father();
+            son s = (son)f;
+
+        }
+    }
+}
---
+++ Examples_OO/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_OO
+{
+    class father
+    {
+    }
+    class son : father
+    {
+        public int Age { get; set; }
+    }
+}
