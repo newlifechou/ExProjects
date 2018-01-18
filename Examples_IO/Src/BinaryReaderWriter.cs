59f96c26b7347b551e39af18ed7ce04ffe61dbb1
xs.zhou <newlifechou@outlook.com>
2018-01-18 15:38:14 +0800

    add examples_io to test system.io

---
+++ Examples_IO/Src/BinaryReaderWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Examples_IO.Src
+{
+    /// <summary>
+    /// 该类测试了binary reader and writer
+    /// </summary>
+    class BinaryReaderWriter
+    {
+
+
+
+
+
+        private void WriteIt()
+        {
+
+        }
+        private void ReadIt()
+        {
+
+        }
+    }
+}
