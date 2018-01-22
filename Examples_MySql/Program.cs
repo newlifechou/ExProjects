3dfa8917e20fa1ce95a126b1d942e9b615ca0fdf
xs.zhou <newlifechou@outlook.com>
2018-01-22 23:45:26 +0800

    connect mysql ok

---
+++ Examples_MySql/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Examples_MySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"server=localhost;database=myshop;uid=root;pwd=newlifechou";
+            MySqlConnection con = new MySqlConnection(conStr);
+            try
+            {
+                con.Open();
+                Console.WriteLine("打开mysql连接成功");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+            Console.Read();
+        }
+    }
+}
