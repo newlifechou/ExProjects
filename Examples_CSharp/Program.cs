9d2a6cf23c3bf877759e391ef0751d1f915624a8
xs.zhou <newlifechou@outlook.com>
2018-01-27 14:21:22 +0800

    add more chapter classes

--- Examples_CSharp/Program.cs
+++ Examples_CSharp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Examples_CSharp.Src;
 
 namespace Examples_CSharp
 {
@@ -10,6 +11,13 @@ namespace Examples_CSharp
     {
         static void Main(string[] args)
         {
+            Chapter06 ct = new Chapter06();
+            foreach (var item in ct)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ Examples_CSharp/Src/Chapter06.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_CSharp.Src
+{
+    /// <summary>
+    /// yield
+    /// </summary>
+    class Chapter06 : IEnumerable<int>
+    {
+        private int[] data;
+        public Chapter06()
+        {
+            int count = 10;
+            data = new int[count];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i * i;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                yield return data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
