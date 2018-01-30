964cdb62bfd346b1c6d876c65cdfbe2851905fa3
xs.zhou <newlifechou@outlook.com>
2018-01-30 11:10:39 +0800

    add more projects into this solution

--- Examples_CSharp/Src/Chapter09.cs
+++ Examples_CSharp/Src/Chapter09.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq.Expressions;
+using System.Collections;
 
 namespace Examples_CSharp.Src
 {
@@ -25,9 +26,38 @@ namespace Examples_CSharp.Src
             Expression firstArg = Expression.Constant(2);
             Expression secondArg = Expression.Constant(3);
             Expression add = Expression.Add(firstArg, secondArg);
-            Console.WriteLine(add);
+            Func<int> compiled = Expression.Lambda<Func<int>>(add).Compile();
+
+            Console.WriteLine(compiled());
+
+            Expression<Func<int>> return5 = () => 5;
+            Func<int> compiled2 = return5.Compile();
+
+            "hello world".MyLength();
 
         }
+
+        public void Test3()
+        {
+            var list = Enumerable.Range(0, 100);
+            var result = list.Where(i => i % 2 == 0).Select(i => new { Number = i });
+
+            var query = from int i in list
+                        select i;
+
+            ArrayList list2 = new ArrayList() { "hello", "world" };
+            IEnumerable<string> result2 = list2.Cast<String>();
+            IEnumerable<string> result3 = list2.OfType<string>();
+
+            var final2 = result3.Select(str => str.Substring(0, 2));
+
+
+
+
+        }
+
+
+
 
 
     }
