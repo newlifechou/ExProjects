c1c11b1e58b6f37745da8e7ad98316ce697c7eb7
xs.zhou <newlifechou@outlook.com>
2018-03-12 12:05:33 +0800

    add BTreeNode class

---
+++ Examples_ClassicAlgorithm/DataStructure/BTreeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_ClassicAlgorithm.DataStructure
+{
+    public class BTreeNode<T>
+    {
+        public T data;
+        public BTreeNode<T> LChild { get; set; }
+        public BTreeNode<T> RChild { get; set; }
+    }
+}
