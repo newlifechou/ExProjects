099d2b72727c95d3b9073e6c5689f5dadd36d0e7
xs.zhou <newlifechou@outlook.com>
2018-03-14 08:40:19 +0800

    add treenodewithclue

---
+++ Examples_ClassicAlgorithm/DataStructure/Tree/TreeNodeWithClue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_ClassicAlgorithm.DataStructure.Tree
+{
+    class TreeNodeWithClue
+    {
+        public string Data;
+        public TreeNodeWithClue LeftNode;
+        public bool LeftTag;
+        public TreeNodeWithClue RightNode;
+        public bool RightTag;
+    }
+}
