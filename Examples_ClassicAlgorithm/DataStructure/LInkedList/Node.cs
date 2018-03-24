bdebfb339b5f7e756e759a4bc529c22eadcd426e
xs.zhou <newlifechou@outlook.com>
2018-03-24 12:34:38 +0800

    add node class

---
+++ Examples_ClassicAlgorithm/DataStructure/LInkedList/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_ClassicAlgorithm.DataStructure.LInkedList
+{
+
+    public class Node
+    {
+        public Node()
+        {
+
+        }
+        public Node(string data)
+        {
+            Data = data;
+        }
+        public Node(string data,Node next)
+        {
+            Data = data;
+            Next = next;
+        }
+        public Node(string data,Node prev,Node next)
+        {
+            Data = data;
+            Prev = prev;
+            Next = next;
+        }
+
+        public string Data;
+        public Node Prev;
+        public Node Next;
+    }
+}
