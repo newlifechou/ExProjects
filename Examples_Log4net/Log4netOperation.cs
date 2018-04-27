5aad00419e6c2e81cd89f5891a95bece28e4a3a8
xs.zhou <newlifechou@outlook.com>
2018-04-27 22:57:19 +0800

    use log4net

---
+++ Examples_Log4net/Log4netOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace Examples_Log4net
+{
+    class Log4netOperation
+    {
+        public Log4netOperation()
+        {
+            //read config
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public void LogError()
+        {
+            ILog log = log4net.LogManager.GetLogger("logerror");
+            log.Error($"something wrong");
+        }
+
+        public void LogInfo()
+        {
+            ILog log = log4net.LogManager.GetLogger("loginfo");
+            log.Error($"something happend");
+        }
+    }
+}
---
+++ Examples_Log4net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_Log4net
+{
+    /// <summary>
+    /// 这个项目是为了演示log4net的使用
+    /// http://logging.apache.org/log4net/index.html
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new Log4netOperation();
+            logger.LogError();
+            logger.LogInfo();
+
+
+
+
+            Console.WriteLine("Done");
+
+            Console.Read();
+        }
+    }
+}
