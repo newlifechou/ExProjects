f965b3ea01bda6f2303332bf74630130df918e70
xs.zhou <newlifechou@outlook.com>
2018-04-27 23:36:26 +0800

    use process

---
+++ Examples_Process/ProcessOperation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Examples_Process
+{
+    /// <summary>
+    ///Process类的使用 
+    /// </summary>
+    static class ProcessOperation
+    {
+        public static void GetAllProcess()
+        {
+            var process_list = Process.GetProcesses();
+            foreach (var p in process_list)
+            {
+                Console.WriteLine($"{p.ProcessName} {p.Id}");
+            }
+        }
+    }
+}
---
+++ Examples_Process/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_Process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessOperation.GetAllProcess();
+
+
+            Console.Read();
+        }
+    }
+}
