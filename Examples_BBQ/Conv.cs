54bc6f0dc1af9c54fa8536b2d8df9a1bcc2cddac
xs.zhou <newlifechou@outlook.com>
2021-09-16 09:42:41 +0800

    add more project

---
+++ Examples_BBQ/Conv.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples_BBQ
+{
+    public static class Conv
+    {
+        /// <summary>
+        /// from string to int
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int ToInt(string number)
+        {
+            int result;
+            int.TryParse(number, out result);
+            return result;
+        }
+    }
+}
---
+++ Examples_WPF/TitlePanel.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Examples_WPF
+{
+    /// <summary>
+    /// TitlePanel.xaml 的交互逻辑
+    /// </summary>
+    public partial class TitlePanel : UserControl
+    {
+        public TitlePanel()
+        {
+            InitializeComponent();
+        }
+
+        public string MyTitle
+        {
+            set
+            {
+                TxtTitle.Text = value;
+            }
+        }
+
+        public Object MyContent
+        {
+            set
+            {
+                TxtContent.Content = value;
+            }
+        }
+
+    }
+}
